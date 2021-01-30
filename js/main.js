bea4452ec2b4cefb4f70fd622b60cf5a4325b2b3
Ivan Goltyaev <goltyaev@icloud.com>
2021-01-30 12:24:14 +0300

    Добавили функцию для возврата случайного числа с плавающей точкой

--- js/main.js
+++ js/main.js
@@ -6,3 +6,9 @@ const GET_INTEGER = function(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+const ARBITRARY_NUMBER = function(min, max, decimalPlaces) {
+  if (max <= min || max <= 0 || min < 0) {
+    return 'Некорректный ввод'
+  }
+  return ((Math.random() * (max - min + 1)) + min).toFixed(decimalPlaces)
+}
