ef713f68ce40298ba27d23b53a54c72ab77cafe3
Ivan Goltyaev <goltyaev@icloud.com>
2021-02-06 11:08:51 +0300

    Вынесли константы в начало кода, удалили лишние вызовы функций

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const NUMBER_OF_OBJECTS = 10;
+const TYPES_ARRAY = ['palace', 'flat', 'house', 'bungalow'];
+const CHECKIN_ARRAY = ['12:00', '13:00', '14:00'];
+const CHECKOUT_ARRAY = ['12:00', '13:00', '14:00'];
+const FEATURES_ARRAY = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS_ARRAY = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg', 
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
 const getInteger = (min, max) => {
   if (max <= min || max <= 0 || min < 0) {
     return 'Некорректный ввод'
@@ -12,41 +22,29 @@ const getArbitraryNumber = (min, max, decimalPlaces) => {
   }
   return (Math.random() * (max - min) + min).toFixed(decimalPlaces);
 }
-getInteger();
-getArbitraryNumber()
-
-
+const getRandomArrayElement = (elements) => {
+  return elements[getInteger(0, elements.length -1)]
+}
 
 const getObject = () => {
-  const typesArray = ['palace', 'flat', 'house', 'bungalow'];
-  const checkinArray = ['12:00', '13:00', '14:00'];
-  const checkoutArray = ['12:00', '13:00', '14:00'];
-  const featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  const photosArray = [
-    'http://o0.github.io/assets/images/tokyo/hotel1.jpg', 
-    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
   const location = {
     x: getArbitraryNumber(35.65000, 35.70000, 5),
     y: getArbitraryNumber(139.70000, 139.80000, 5),
   };
-  const getRandomArrayElement = (elements) => {
-    return elements[getInteger(0, elements.length -1)]
-  }
   return {
     author: {avatar: 'img/avatars/user' + '0' + getInteger(1, 8) + '.png'},
     offer: {
       title: 'Заголовок',
       address: location.x + ', ' + location.y,
       price: getInteger(2000, 5000),
-      type: getRandomArrayElement(typesArray),
+      type: getRandomArrayElement(TYPES_ARRAY),
       rooms: getInteger(1, 5),
       guests: getInteger(1, 5),
-      checkin: getRandomArrayElement(checkinArray),
-      checkout: getRandomArrayElement(checkoutArray),
-      features: featuresArray.slice(getInteger(0, featuresArray.length -1 )),
+      checkin: getRandomArrayElement(CHECKIN_ARRAY),
+      checkout: getRandomArrayElement(CHECKOUT_ARRAY),
+      features: FEATURES_ARRAY.slice(getInteger(0, FEATURES_ARRAY.length -1 )),
       description: 'Описание помещения',
-      photos: photosArray.slice(getInteger(0, photosArray.length -1 )),
+      photos: PHOTOS_ARRAY.slice(getInteger(0, PHOTOS_ARRAY.length -1 )),
     },
     location: {
       x: location.x,
@@ -54,9 +52,7 @@ const getObject = () => {
     },
   }
 };
-getObject();
 
-const NUMBER_OF_OBJECTS = 10;
 const getArray = () => {
   let array = [];
   for(let i = 0; i < NUMBER_OF_OBJECTS; i++) {
